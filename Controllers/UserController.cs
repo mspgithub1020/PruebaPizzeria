96997c4e25987d20484ae0f76491045b1c3b0ff6
mspgithub1020 <mspformacion1020@gmail.com>
2020-02-24 02:33:24 +0100

    primer paso

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PruebaPizzeria.Aplication;
+using PruebaPizzeria.DTO.Usuario;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] UserRegistration userRegistration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            UserRegistered userRegistered = _userService.Register(userRegistration);
+            // TODO: return Created
+            return Ok(userRegistered);
+        }
+    }
+}
